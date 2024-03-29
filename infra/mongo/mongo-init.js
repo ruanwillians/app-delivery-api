3815f89f678011d0bc61bb44be4524f777f8f8b2
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2024-03-29 11:03:17 -0300

    Feat(database): add conection database

--- infra/mongo/mongo-init.js
+++ infra/mongo/mongo-init.js
@@ -3,8 +3,8 @@ const conn = new Mongo();
 const adminDB = conn.getDB('admin');
 adminDB.auth('helpdev', '123456');
 
-const codeBurgerDB = conn.getDB('code-burger');
+const codeBurgerDB = conn.getDB('code-delivery');
 
 codeBurgerDB.createCollection('user');
 
-print("Banco de dados 'code-burger' e coleção 'products' criados.");
+print("Banco de dados 'code-delivery' e coleção 'products' criados.");
