19f99cb25add1338e05daa0fecfa10dee9ff7e63
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2024-03-29 01:09:10 -0300

    test husky

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello Worl');
+      expect(appController.getHello()).toBe('Hello World!');
     });
   });
 });
