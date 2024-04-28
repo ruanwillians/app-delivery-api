ee0ceddd2d3dbfa4f03c100574ffbff3ff9124fa
Ruan Willians Mira Nogueira <108192932+ruanwillians@users.noreply.github.com>
2024-04-28 09:35:20 -0300

    Fix: compile ci cd

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -5,11 +5,11 @@ dotenv.config(); // Carrega as variáveis de ambiente do arquivo .env
 
 export const dbdatasource: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrationsTableName: 'migration',
   migrations: ['dist/db/migrations/*.js'],
